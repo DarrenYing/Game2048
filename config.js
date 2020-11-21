4f9740336532f4ed36a2bf15ad56f8f4fcc6f7e6
DarrenYing <38713820+DarrenYing@users.noreply.github.com>
2020-11-21 21:14:02 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,46 @@
+let colorsSizes = {
+  '2': {
+    size: 64,
+    color: '#ffaf40'
+  },
+  '4': {
+    size: 64,
+    color: '#2d98da'
+  },
+  '8': {
+    size: 64,
+    color: '#cd84f1'
+  },
+  '16': {
+    size: 64,
+    color: '#0fb9b1'
+  },
+  '32': {
+    size: 64,
+    color: '#18dcff'
+  },
+  '64': {
+    size: 64,
+    color: '#32ff7e'
+  },
+  '128': {
+    size: 36,
+    color: '#ff4d4d'
+  },
+  '256': {
+    size: 36,
+    color: '#fff200'
+  },
+  '512': {
+    size: 36,
+    color: '#ffb8b8'
+  },
+  '1024': {
+    size: 64,
+    color: '#4b4b4b'
+  },
+  '2048': {
+    size: 64,
+    color: '#ff3838'
+  }
+};
